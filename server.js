9b52c13260ff57949447de5c7b06c5f5db093652
Shantun Singh Parmar <55224162+shantunparmar@users.noreply.github.com>
2021-10-20 15:14:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World!');
-}).listen(8080);
+}).listen(process.env.PORT);
